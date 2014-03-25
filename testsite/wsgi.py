2ad1e42b72dcb66ffc91354943d80efc12b6438c
Sebastien Mirolo <smirolo@fortylines.com>
2014-03-25 10:07:01 -0700

    clean pylint report on testsite

--- testsite/wsgi.py
+++ testsite/wsgi.py
@@ -11,4 +11,4 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "testsite.settings")
 
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+application = get_wsgi_application() #pylint: disable=invalid-name
