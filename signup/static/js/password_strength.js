1da6653bbc3846374a89e72fe74bdf3e0a4f8cbd
Stephane Robino <stephane.robino@gmail.com>
2015-04-09 23:11:01 +0200

    Password strenght js to plugin

--- signup/static/js/password_strength.js
+++ signup/static/js/password_strength.js
@@ -1,21 +1,5 @@
 /*jshint multistr: true */
-
-$(document).ready(function() {
-    var $input_password = $('input[type="password"]').filter(function(){
-        return this.id.match("password$") || this.id.match("password1$");
-    });
-
-    var $popover = $input_password.popover({
-        content: '<ul class="list-unstyled" style="padding:0">\
-        <li class="text-danger" id="pass_length">8 characters min. <i class="fa fa-check" style="visibility:hidden"></i></li>\
-        <li class="text-danger" id="pass_letter">1 letter <i class="fa fa-check" style="visibility:hidden"></i></li>\
-        <li class="text-danger" id="pass_number">1 number <i class="fa fa-check" style="visibility:hidden"></i></li>\
-        <li class="text-danger" id="pass_cap_letter">1 cap letter <i class="fa fa-check" style="visibility:hidden"></i></li>\
-        </ul>',
-        html:true,
-        placement: 'bottom',
-        trigger: 'focus',
-    });
+(function($){
 
     function pass_test(element){
         element.removeClass('text-danger').addClass('text-success');
@@ -27,45 +11,66 @@ $(document).ready(function() {
          element.children('i').css('visibility', 'hidden');
     }
 
-    $('input[type="password"]').keyup(function(){
-        var value = $(this).val();
-        var valid_password = true;
-        if (value.length >= 8){
-            pass_test($('#pass_length'));
-            valid_password = valid_password && true;
-        }else{
-            not_pass_test($('#pass_length'));
-            valid_password = valid_password && false;
-        }
-        if (/[a-z]/.test(value)){
-            pass_test($('#pass_letter'));
-            valid_password = valid_password && true;
-        }else{
-            not_pass_test($('#pass_letter'));
-            valid_password = valid_password && false;
-        }
-        if (/[A-Z]/.test(value)){
-            pass_test($('#pass_cap_letter'));
-            valid_password = valid_password && true;
-        }else{
-            not_pass_test($('#pass_cap_letter'));
-            valid_password = valid_password && false;
-        }
-        if (/[0-9]/.test(value)){
-            pass_test($('#pass_number'));
-            valid_password = valid_password && true;
-        }else{
-            not_pass_test($('#pass_number'));
-            valid_password = valid_password && false;
-        }
-        if (valid_password){
-            $('input[type="submit"]').attr('disabled', false);
-        }else{
-            $('input[type="submit"]').attr('disabled', true);
-        }
-    });
 
-    $popover.on('shown.bs.popover', function(){
-        $input_password.trigger('keyup');
-    });
-});
+    $.fn.passwordStrength = function(options){
+        var opts = $.extend( {}, $.fn.passwordStrength.defaults, options );
+        var _this = $(this);
+
+        var $popover = _this.popover({
+            content: '<ul class="list-unstyled" style="padding:0">\
+            <li class="text-danger" id="pass_length">8 characters min. <i class="fa fa-check" style="visibility:hidden"></i></li>\
+            <li class="text-danger" id="pass_letter">1 letter <i class="fa fa-check" style="visibility:hidden"></i></li>\
+            <li class="text-danger" id="pass_number">1 number <i class="fa fa-check" style="visibility:hidden"></i></li>\
+            <li class="text-danger" id="pass_cap_letter">1 cap letter <i class="fa fa-check" style="visibility:hidden"></i></li>\
+            </ul>',
+            html:true,
+            placement: 'bottom',
+            trigger: 'focus',
+        });
+
+        _this.keyup(function(){
+            var value = $(this).val();
+            var valid_password = true;
+            if (value.length >= 8){
+                pass_test($('#pass_length'));
+                valid_password = valid_password && true;
+            }else{
+                not_pass_test($('#pass_length'));
+                valid_password = valid_password && false;
+            }
+            if (/[a-z]/.test(value)){
+                pass_test($('#pass_letter'));
+                valid_password = valid_password && true;
+            }else{
+                not_pass_test($('#pass_letter'));
+                valid_password = valid_password && false;
+            }
+            if (/[A-Z]/.test(value)){
+                pass_test($('#pass_cap_letter'));
+                valid_password = valid_password && true;
+            }else{
+                not_pass_test($('#pass_cap_letter'));
+                valid_password = valid_password && false;
+            }
+            if (/[0-9]/.test(value)){
+                pass_test($('#pass_number'));
+                valid_password = valid_password && true;
+            }else{
+                not_pass_test($('#pass_number'));
+                valid_password = valid_password && false;
+            }
+            if (valid_password){
+                $('input[type="submit"]').attr('disabled', false);
+            }else{
+                $('input[type="submit"]').attr('disabled', true);
+            }
+        });
+        $popover.on('shown.bs.popover', function(){
+            _this.trigger('keyup');
+        });
+        _this.trigger('keyup');
+    };
+
+    $.fn.passwordStrength.defaults = {};
+
+}(jQuery));
