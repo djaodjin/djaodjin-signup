797e2b22426a74d2564dce6707a677083a104aa8
Sebastien Mirolo <smirolo@djaodjin.com>
2018-05-20 22:59:56 -0700

    reverse order of exception handler for Python/Django compatibility

--- signup/compat.py
+++ signup/compat.py
@@ -38,10 +38,10 @@ User.objects.model = User
 
 try:
     from django.urls import NoReverseMatch, reverse, reverse_lazy
+except ImportError: # <= Django 1.10, Python<3.6
+    from django.core.urlresolvers import NoReverseMatch, reverse, reverse_lazy
 except ModuleNotFoundError: #pylint:disable=undefined-variable
     # <= Django 1.10, Python>=3.6
-    from django.core.urlresolvers import NoReverseMatch, reverse, reverse_lazy
-except ImportError: # <= Django 1.10, Python<3.6
     from django.core.urlresolvers import NoReverseMatch, reverse, reverse_lazy
 
 # We would use:
