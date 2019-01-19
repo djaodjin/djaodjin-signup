a3ab17d4ac4de4dbdd34bb839cb205a175ccc5f8
Dima Knivets <knivets@gmail.com>
2019-01-20 02:25:13 +0700

    removed dummy settings

--- testsite/settings.py
+++ testsite/settings.py
@@ -199,12 +199,3 @@ DEBUG_TOOLBAR_CONFIG = {
 INTERNAL_IPS = ('127.0.0.1', '::1')
 
 NOCAPTCHA = True
-
-if not DEBUG:
-    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
-#required by django-storages
-#AWS_STORAGE_BUCKET_NAME
-#AWS_ACCESS_KEY_ID
-#AWS_SECRET_ACCESS_KEY
-#AWS_DEFAULT_ACL = None
