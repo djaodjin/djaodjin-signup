186cd027df238f8b1e8d53e4551673c951c7dede
Sebastien Mirolo <smirolo@djaodjin.com>
2019-01-31 17:42:53 -0800

    releases 0.2.7 to pypi

--- signup/__init__.py
+++ signup/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2018, Djaodjin Inc.
+# Copyright (c) 2019, Djaodjin Inc.
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -26,4 +26,4 @@
 PEP 386-compliant version number for the signup django app.
 """
 
-__version__ = '0.2.7-dev'
+__version__ = '0.2.7'
