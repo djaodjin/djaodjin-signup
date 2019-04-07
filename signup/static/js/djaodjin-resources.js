cd078a807cdf3736a87698e263e7b6d4d351aee4
Dima Knivets <knivets@gmail.com>
2019-04-07 17:27:52 +0300

    i18n

--- signup/static/js/djaodjin-resources.js
+++ signup/static/js/djaodjin-resources.js
@@ -56,7 +56,7 @@ function _showErrorMessages(resp) {
         var data = resp.data || resp.responseJSON;
         if( data && typeof data === "object" ) {
             if( data.detail ) {
-                messages = ["Error: " + data.detail];
+                messages = [gettext("Error: ") + data.detail];
             } else if( $.isArray(data) ) {
                 for( var idx = 0; idx < data.length; ++idx ) {
                     messages = messages.concat(_showErrorMessages(data[idx]));
@@ -87,7 +87,7 @@ function _showErrorMessages(resp) {
                 }
             }
         } else if( resp.detail ) {
-            messages = ["Error: " + resp.detail];
+            messages = [gettext("Error: ") + resp.detail];
         }
     }
     return messages;
@@ -96,9 +96,9 @@ function _showErrorMessages(resp) {
 
 function showErrorMessages(resp) {
     if( resp.status >= 500 && resp.status < 600 ) {
-        messages = ["Error " + resp.status + ": " + resp.statusText
+        messages = [interpolate(gettext("Error %s: %s" +
             + ". We have been notified and have started on fixing"
-            + " the error. We apologize for the inconvinience."];
+            + " the error. We apologize for the inconvinience."), resp.status, resp.statusText)];
     } else {
         var messages = _showErrorMessages(resp);
         if( messages.length === 0 ) {
