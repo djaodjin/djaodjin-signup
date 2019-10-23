djaodjin/djaodjin-signup

commit ca815635c9ca3f882ac7bdc16c013c8b0251fbd1

corrects multiple updates issues
- upload of contact picture
- use full_name in user profile instead of first_name/last_name
- use inline-template for Vue.js components

signup/api/auth.py
@@ -49,7 +49,6 @@ from ..models import Contact
 from ..serializers import (CredentialsSerializer, CreateUserSerializer,
     TokenSerializer, UserSerializer, ValidationErrorSerializer,
     PasswordResetSerializer)
-from ..utils import verify_token as verify_token_base
 
 
 LOGGER = logging.getLogger(__name__)
@@ -195,20 +194,20 @@ JwcBUUMECj8AKxsHtRHUSypco"
                     'This email address has already been registered!'\
 ' Please <a href="%s">login</a> with your credentials. Thank you.'
                     % reverse('login'))))
-            else:
-                ValidationError(mark_safe(_(
-                    "This email address has already been registered!"\
+            raise ValidationError(mark_safe(_(
+                "This email address has already been registered!"\
 " You should now secure and activate your account following "\
 " the instructions we just emailed you. Thank you.")))
-            return None
 
-        first_name, mid_initials, last_name = full_name_natural_split(
-            validated_data['full_name'])
+        first_name, mid_name, last_name = full_name_natural_split(
+            validated_data['full_name'], mid_initials=True)
+        if mid_name:
+            first_name = (first_name + " " + mid_name).strip()
         username = validated_data.get('username', None)
         password = validated_data.get('password', None)
         user = self.model.objects.create_user(username,
             email=email, password=password,
-            first_name=first_name + " " + mid_initials, last_name=last_name)
+            first_name=first_name, last_name=last_name)
 
         # Bypassing authentication here, we are doing frictionless registration
         # the first time around.

signup/templates/users/password.html
@@ -1,7 +1,7 @@
 {% extends "base.html" %}
 
 {% block content %}
-<div id="update-password-container">
+<user-update-password id="update-password-container" inline-template>
     <h1>Change password</h1>
     <form @submit.prevent="updatePassword">
         <li>
@@ -18,5 +18,5 @@
             <input type="submit" name="submit" value="Submit" id="submit-id-submit">
         </div>
     </form>
-</div>
+</user-update-password>
 {% endblock %}

signup/views/auth.py
@@ -99,11 +99,18 @@ class AuthTemplateResponseMixin(UrlsMixin, TemplateResponseMixin):
             **kwargs)
         # URLs for user
         disabled_registration = get_disabled_registration(self.request)
-        self.update_context_urls(context, {'user': {
-           'login': reverse('login'),
-           'password_reset': reverse('password_reset'),
-           'register': (reverse('registration_register')
-                if not disabled_registration else None),
+        self.update_context_urls(context, {
+            'api': {
+                'login': reverse('api_login'),
+                'recover': reverse('api_recover'),
+                'register': (reverse('api_register')
+                    if not disabled_registration else None),
+            },
+            'user': {
+                'login': reverse('login'),
+                'password_reset': reverse('password_reset'),
+                'register': (reverse('registration_register')
+                    if not disabled_registration else None),
         }})
         return context
 
@@ -291,16 +298,14 @@ class SignupBaseView(RedirectFormMixin, ProcessFormView):
                          "This email address has already been registered!"\
 " Please <a href=\"%s\">login</a> with your credentials. Thank you.")
                         % reverse('login'))})
-            else:
-                raise ValidationError(
-                    {'email':
-                     _("A user with that e-mail address already exists."),
-                    api_settings.NON_FIELD_ERRORS_KEY:
-                     mark_safe(_(
-                         "This email address has already been registered!"\
+            raise ValidationError(
+                {'email':
+                 _("A user with that e-mail address already exists."),
+                 api_settings.NON_FIELD_ERRORS_KEY:
+                 mark_safe(_(
+                     "This email address has already been registered!"\
 " You should now secure and activate your account following "\
 " the instructions we just emailed you. Thank you."))})
-            return None
 
         first_name, last_name = self.first_and_last_names(**cleaned_data)
         username = cleaned_data.get('username', None)

signup/api/contacts.py
@@ -24,16 +24,19 @@
 
 """APIs for profiles and profile activities"""
 
-import logging
-from hashlib import sha256
+import hashlib, logging, os
 
-from rest_framework import filters
+from django.contrib.auth import get_user_model
+from django.db import transaction
+from django.utils.encoding import force_text
+from rest_framework import filters, parsers, status
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView
+from rest_framework.response import Response
 
 from .users import UserDetailAPIView, UserListCreateAPIView
 from ..mixins import ContactMixin
 from ..models import Activity, Contact
-from ..serializers import ActivitySerializer, ContactPictureSerializer
+from ..serializers import ActivitySerializer
 from ..utils import get_picture_storage
 
 
@@ -136,16 +139,42 @@ class ContactListAPIView(UserListCreateAPIView):
 
 
 class ContactPictureAPIView(ContactMixin, RetrieveUpdateAPIView):
-    serializer_class = ContactPictureSerializer
-    queryset = Contact.objects.all().select_related('user')
+    """
+        Uploads a static asset file
 
-    def put(self, request, *args, **kwargs):
-        storage = get_picture_storage()
-        picture = request.data.get('picture')
-        if picture:
-            name = '%s.%s' % (sha256(picture.read()).hexdigest(), 'jpg')
-            storage.save(name, picture)
-            request.data['picture'] = storage.url(name)
-        return self.update(request, *args, **kwargs)
+        **Examples
 
-    patch = put
+        .. code-block:: http
+
+            POST /api/contacts/xia/picture/ HTTP/1.1
+    """
+    parser_classes = (parsers.FormParser, parsers.MultiPartParser)
+    user_queryset = get_user_model().objects.filter(is_active=True)
+
+    def post(self, request, *args, **kwargs):
+        #pylint:disable=unused-argument
+        uploaded_file = request.data.get('file')
+        if not uploaded_file:
+            return Response({'details': "no location or file specified."},
+                status=status.HTTP_400_BAD_REQUEST)
+
+        # tentatively extract file extension.
+        parts = os.path.splitext(
+            force_text(uploaded_file.name.replace('\\', '/')))
+        ext = parts[-1].lower() if len(parts) > 1 else ""
+        key_name = "%s%s" % (
+            hashlib.sha256(uploaded_file.read()).hexdigest(), ext)
+        default_storage = get_picture_storage()
+        location = self.request.build_absolute_uri(default_storage.url(
+            default_storage.save(key_name, uploaded_file)))
+        user_model = self.user_queryset.model
+        with transaction.atomic():
+            try:
+                user = user_model.objects.get(
+                    username=self.kwargs.get(self.lookup_url_kwarg))
+            except user_model.DoesNotExist:
+                user = None
+            Contact.objects.update_or_create(
+                slug=self.kwargs.get(self.lookup_url_kwarg),
+                defaults={'picture': location, 'user': user})
+        return Response({'location': location}, status=status.HTTP_201_CREATED)

signup/api/users.py
@@ -23,7 +23,6 @@
 # ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 import logging, re
-from hashlib import sha256
 
 from django.contrib.auth import logout as auth_logout
 from django.db import transaction, IntegrityError
@@ -45,7 +44,7 @@ from ..models import Contact
 from ..serializers import (ContactSerializer, ContactDetailSerializer,
     PasswordChangeSerializer, NoModelSerializer, NotificationsSerializer,
     ValidationErrorSerializer)
-from ..utils import get_picture_storage, generate_random_code, handle_uniq_error
+from ..utils import generate_random_code, handle_uniq_error
 
 
 LOGGER = logging.getLogger(__name__)
@@ -190,13 +189,39 @@ class UserDetailAPIView(ContactMixin, RetrieveUpdateDestroyAPIView):
               "nick_name": "Xia"
             }
         """
-        storage = get_picture_storage()
-        picture = request.data.get('picture')
-        if picture:
-            name = '%s.%s' % (sha256(picture.read()).hexdigest(), 'jpg')
-            storage.save(name, picture)
-            request.data['picture'] = storage.url(name)
         return self.update(request, *args, **kwargs)
+
+    def patch(self, request, *args, **kwargs):
+        """
+        Updates a login profile
+
+        **Tags: profile
+
+        **Examples
+
+        .. code-block:: http
+
+            PATCH /api/users/xia/ HTTP/1.1
+
+        .. code-block:: json
+
+            {
+              "email": "xia@locahost.localdomain",
+              "full_name": "Xia Lee",
+              "nick_name": "Xia"
+            }
+
+        responds
+
+        .. code-block:: json
+
+            {
+              "email": "xia@locahost.localdomain",
+              "full_name": "Xia Lee",
+              "nick_name": "Xia"
+            }
+        """
+        return self.partial_update(request, *args, **kwargs)
 
     def delete(self, request, *args, **kwargs):
         """
@@ -260,11 +285,14 @@ class UserDetailAPIView(ContactMixin, RetrieveUpdateDestroyAPIView):
                     if serializer.validated_data.get('slug'):
                         user.username = serializer.validated_data.get('slug')
                     if serializer.validated_data.get('full_name'):
-                        #pylint:disable=unused-variable
                         first_name, mid_name, last_name = \
                             full_name_natural_split(
-                                serializer.validated_data.get('full_name'))
+                                serializer.validated_data.get('full_name'),
+                                middle_initials=False)
                         user.first_name = first_name
+                        if mid_name:
+                            user.first_name = (
+                                first_name + " " + mid_name).strip()
                         user.last_name = last_name
                     user.save()
                 serializer.save()
@@ -340,6 +368,14 @@ class UserListCreateAPIView(ListCreateAPIView):
     queryset = Contact.objects.all().select_related('user')
     user_queryset = get_user_model().objects.filter(is_active=True)
 
+    def get_serializer_class(self):
+        if self.request.method.lower() == 'post':
+            return ContactDetailSerializer
+        return super(UserListCreateAPIView, self).get_serializer_class()
+
+    @swagger_auto_schema(request_body=ContactDetailSerializer, responses={
+        201: OpenAPIResponse("success", ContactDetailSerializer),
+        400: OpenAPIResponse("parameters error", ValidationErrorSerializer)})
     def post(self, request, *args, **kwargs):
         """
         Creates a user profile

signup/compat.py
@@ -33,7 +33,7 @@ try:
     from django.urls import NoReverseMatch, reverse, reverse_lazy
 except ImportError: # <= Django 1.10, Python<3.6
     from django.core.urlresolvers import NoReverseMatch, reverse, reverse_lazy
-except ModuleNotFoundError: #pylint:disable=undefined-variable
+except ModuleNotFoundError: #pylint:disable=undefined-variable,bad-except-order
     # <= Django 1.10, Python>=3.6
     from django.core.urlresolvers import NoReverseMatch, reverse, reverse_lazy
 

signup/decorators.py
@@ -30,6 +30,7 @@ from functools import wraps
 
 from django.contrib import messages
 from django.contrib.auth import (REDIRECT_FIELD_NAME, logout as auth_logout)
+from django.contrib.auth.views import redirect_to_login
 from django.core.exceptions import PermissionDenied
 from django.utils.decorators import available_attrs
 from django.utils import six
@@ -57,7 +58,6 @@ def _insert_url(request, redirect_field_name=REDIRECT_FIELD_NAME,
     if ((not login_scheme or login_scheme == current_scheme) and
         (not login_netloc or login_netloc == current_netloc)):
         path = request.get_full_path()
-    from django.contrib.auth.views import redirect_to_login
     return redirect_to_login(path, inserted_url, redirect_field_name)
 
 

commit b111ea6bf157571a21f43fb389a09880e376eebc

fixes regression introduced by commit ca815635

signup/api/auth.py
@@ -200,7 +200,7 @@ JwcBUUMECj8AKxsHtRHUSypco"
 " the instructions we just emailed you. Thank you.")))
 
         first_name, mid_name, last_name = full_name_natural_split(
-            validated_data['full_name'], mid_initials=True)
+            validated_data['full_name'], middle_initials=False)
         if mid_name:
             first_name = (first_name + " " + mid_name).strip()
         username = validated_data.get('username', None)

signup/static/js/djaodjin-signup-vue.js
@@ -758,56 +758,6 @@ Vue.component('user-update', {
                 showErrorMessages(resp);
             });
         },
-        get: function(){
-            var vm = this;
-            vm.reqGet(djaodjinSettings.urls.user.api_profile,
-            function(resp) {
-                vm.formFields = resp;
-            });
-        },
-        validateForm: function(){ // XXX depends on jQuery
-            var vm = this;
-            var isEmpty = true;
-            var fields = $(vm.$el).find('[name]').not(
-                '[name="csrfmiddlewaretoken"]');
-            for( var fieldIdx = 0; fieldIdx < fields.length; ++fieldIdx ) {
-                var fieldName = $(fields[fieldIdx]).attr('name');
-                var fieldValue = $(fields[fieldIdx]).val();
-                if( vm.formFields[fieldName] !== fieldValue ) {
-                    vm.formFields[fieldName] = fieldValue;
-                }
-                if( vm.formFields[fieldName] ) {
-                    // We have at least one piece of information
-                    // about the plan already available.
-                    isEmpty = false;
-                }
-            }
-            return !isEmpty;
-        },
-        updateProfile: function(){
-            var vm = this;
-            vm.validateForm();
-            vm.reqPatch(djaodjinSettings.urls.user.api_profile, vm.formFields,
-            function(resp) {
-                // XXX should really be success but then it needs to be changed
-                // in Django views as well.
-                showMessages([gettext("Profile updated.")], "info");
-            }, function(resp){
-                showErrorMessages(resp);
-            });
-            if(vm.imageSelected){
-                vm.uploadProfilePicture();
-            }
-        },
-        deleteProfile: function() {
-            var vm = this;
-            vm.reqDelete(djaodjinSettings.urls.user.api_profile,
-            function() {
-                window.location = djaodjinSettings.urls.user.profile_redirect;
-            }, function(resp){
-                showErrorMessages(resp);
-            });
-        },
         resetKey: function(){
             this.apiModalOpen = true;
         },
@@ -826,6 +776,37 @@ Vue.component('user-update', {
                 }
                 showErrorMessages(resp);
             });
+        },
+        deleteProfile: function() {
+            var vm = this;
+            vm.reqDelete(djaodjinSettings.urls.user.api_profile,
+            function() {
+                window.location = djaodjinSettings.urls.user.profile_redirect;
+            }, function(resp){
+                showErrorMessages(resp);
+            });
+        },
+        get: function(){
+            var vm = this;
+            vm.reqGet(djaodjinSettings.urls.user.api_profile,
+            function(resp) {
+                vm.formFields = resp;
+            });
+        },
+        updateProfile: function(){
+            var vm = this;
+            vm.validateForm();
+            vm.reqPatch(djaodjinSettings.urls.user.api_profile, vm.formFields,
+            function(resp) {
+                // XXX should really be success but then it needs to be changed
+                // in Django views as well.
+                showMessages([gettext("Profile updated.")], "info");
+            }, function(resp){
+                showErrorMessages(resp);
+            });
+            if(vm.imageSelected){
+                vm.uploadProfilePicture();
+            }
         },
         uploadProfilePicture: function() {
             var vm = this;
@@ -864,10 +845,29 @@ Vue.component('user-update', {
                 });
             }, 'image/jpeg');
         },
+        validateForm: function(){ // XXX depends on jQuery
+            var vm = this;
+            var isEmpty = true;
+            var fields = $(vm.$el).find('[name]').not(
+                '[name="csrfmiddlewaretoken"]');
+            for( var fieldIdx = 0; fieldIdx < fields.length; ++fieldIdx ) {
+                var fieldName = $(fields[fieldIdx]).attr('name');
+                var fieldValue = $(fields[fieldIdx]).val();
+                if( vm.formFields[fieldName] !== fieldValue ) {
+                    vm.formFields[fieldName] = fieldValue;
+                }
+                if( vm.formFields[fieldName] ) {
+                    // We have at least one piece of information
+                    // about the plan already available.
+                    isEmpty = false;
+                }
+            }
+            return !isEmpty;
+        },
     },
     computed: {
         imageSelected: function(){
-            return this.picture.hasImage();
+            return this.picture && this.picture.hasImage();
         }
     },
     mounted: function(){

signup/templates/users/password.html
@@ -2,6 +2,7 @@
 
 {% block content %}
 <user-update-password id="update-password-container" inline-template>
+  <div>
     <h1>Change password</h1>
     <form @submit.prevent="updatePassword">
         <li>
@@ -18,5 +19,6 @@
             <input type="submit" name="submit" value="Submit" id="submit-id-submit">
         </div>
     </form>
+  </div>
 </user-update-password>
 {% endblock %}