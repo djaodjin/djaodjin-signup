a791290f11012e5a0197d1e87e0bbdeaf498b36b
Sebastien Mirolo <smirolo@djaodjin.com>
2020-01-22 09:27:39 -0800

    bumps version number to 0.4.4-dev

--- signup/__init__.py
+++ signup/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2019, Djaodjin Inc.
+# Copyright (c) 2020, Djaodjin Inc.
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -26,4 +26,4 @@
 PEP 386-compliant version number for the signup django app.
 """
 
-__version__ = '0.4.3'
+__version__ = '0.4.4-dev'
