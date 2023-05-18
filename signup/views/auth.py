djaodjin/djaodjin-signup

commit e9e3b7968446c5018dd3c7175535112fc9161f7e

fixes 500 posting to incorrect URL

signup/views/auth.py
@@ -114,7 +114,8 @@ class RecoverView(VerifyMixin, AuthResponseMixin, View):
         try:
             self.run_pipeline()
             return HttpResponseRedirect(self.get_success_url())
-        except serializers.ValidationError as err:
+        except (serializers.ValidationError,
+                exceptions.AuthenticationFailed) as err:
             form = self.get_form()
             fill_form_errors(form, err)
             context = self.get_context_data(form=form)

signup/mixins.py
@@ -144,7 +144,10 @@ class AuthMixin(object):
                 extra =  look.group('extra')
                 if extra:
                     raise IncorrectPath(
-                        self.request.build_absolute_uri(expected_path))
+                        {'detail': (
+                         _("Incorrect path in URL. Expecting %(path)s") % {
+                        'path': self.request.build_absolute_uri(expected_path)}
+                    )})
         except AttributeError:
             pass # Django<=1.11 ResolverMatch does not have
                  # a route attribute.
@@ -217,7 +220,8 @@ class AuthMixin(object):
     def auth_check_disabled(self, user):
         auth_disabled = get_disabled_authentication(self.request, user)
         if auth_disabled:
-            raise AuthDisabled()
+            raise AuthDisabled(
+                {'detail': _("Authentication is disabled")})
 
     def check_user_throttles(self, request, user):
         """
@@ -426,7 +430,8 @@ class RegisterMixin(AuthMixin):
     def register_check_disabled(self):
         disabled_registration = get_disabled_registration(self.request)
         if disabled_registration:
-            raise RegistrationDisabled()
+            raise RegistrationDisabled(
+                {'detail': _("Registration is disabled")})
 
     def register_check_data(self, **cleaned_data):
         email = cleaned_data.get('email')

commit 270e35c1e77bbdb19680918397aa0d9e2f3a80e3

fixes regression introduced by commit e9e3b796 with recover and SSO

signup/views/auth.py
@@ -114,8 +114,7 @@ class RecoverView(VerifyMixin, AuthResponseMixin, View):
         try:
             self.run_pipeline()
             return HttpResponseRedirect(self.get_success_url())
-        except (serializers.ValidationError,
-                exceptions.AuthenticationFailed) as err:
+        except serializers.ValidationError as err:
             form = self.get_form()
             fill_form_errors(form, err)
             context = self.get_context_data(form=form)
@@ -129,7 +128,10 @@ class RecoverView(VerifyMixin, AuthResponseMixin, View):
             context = self.get_context_data(form=form)
         except AuthDisabled:
             context = {'disabled_authentication': True}
-
+        except exceptions.AuthenticationFailed as err:
+            form = self.get_form()
+            fill_form_errors(form, err)
+            context = self.get_context_data(form=form)
         if not context:
             context = self.get_context_data()
         return self.render_to_response(context)