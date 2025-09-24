51f0334440b7c194fdf64e13f7f848c978ac755d
Sebastien Mirolo <smirolo@djaodjin.com>
2025-09-23 20:51:42 -0700

    releases 0.10.1 to pypi

--- README.md
+++ README.md
@@ -69,7 +69,7 @@ Tested with
 - **Python:** 3.12, **Django:** 5.1 (next)
 - **Python:** 3.7, **Django:** 3.2 (legacy)
 
-0.10.0
+0.10.1
 
   * enables use of codes or links to verify channels (email, phone)
   * adds APIs to create account activities
--- docs/conf.py
+++ docs/conf.py
@@ -35,7 +35,7 @@ author = 'The DjaoDjin Team'
 # The short X.Y version
 version = '0.10'
 # The full version, including alpha/beta/rc tags
-release = '0.10.1-dev'
+release = '0.10.1'
 
 
 # -- General configuration ---------------------------------------------------
